96d953ed2cd21c9bde6068fd61716bd145845fa5
levent1360 <levent1360@gmail.com>
2024-10-10 16:41:12 +0300

    Update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f224e34e8788d104fd415bda7958f5c777156e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd96dc1f720a8a2df2dd3f314719cd21471bd6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(o => o.UseNpgsql(config.GetConnectionString("PostgresConnection")));
+
+            return services;
+        }
+    }
+}
